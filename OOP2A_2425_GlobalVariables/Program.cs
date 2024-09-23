92d8a9300f9a5c58d9730e8dd3560da69d644b30
Julian Laspona <JulianDevAcc@hotmail.ph>
2024-09-23 14:42:42 +0800

    Add project files.

---
+++ OOP2A_2425_GlobalVariables/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2A_2425_GlobalVariables
+{
+    internal class Program
+    {
+        /// <summary>
+        /// This is a global variable
+        /// </summary>
+        static int num = 4;
+
+        static void Main(string[] args)
+        {
+            int aNumber = 12;
+
+            displayNum();
+            num = 14;
+
+            displayNum();
+            justAMethod(23);
+
+            displayNum();
+
+            aNumber = int.Parse("a");
+
+            if(Parse("a", out aNumber))
+            {
+
+            }
+
+            Console.ReadKey();
+        }
+
+        static void justAMethod(int a)
+        {
+            Console.WriteLine(a);
+            //num = 15;
+        }
+
+        static void displayNum()
+        {
+            Console.WriteLine(">>>>" + num);
+        }
+
+        static void justAMethod()
+        {
+
+        }
+        static void justAMethod(int a, int b)
+        {
+
+        }
+        static void justAMethod(string a, int b)
+        {
+
+        }
+        static void justAMethod(int a, string b)
+        {
+
+        }
+
+
+        static bool Parse(string input, out int num)
+        {
+            try
+            {
+                num = int.Parse(input);
+                return true;
+            }
+            catch (Exception e)
+            {
+                num = 0;
+                return false;
+            }
+        }
+
+        static bool Parse(string input, out float num)
+        {
+            try
+            {
+                num = float.Parse(input);
+                return true;
+            }
+            catch (Exception e)
+            {
+                num = 0;
+                return false;
+            }
+        }
+    }
+}
---
+++ OOP2A_2425_GlobalVariables/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OOP2A_2425_GlobalVariables")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OOP2A_2425_GlobalVariables")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f6bffdc6-34dc-420b-8eab-453ef854f3f2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
